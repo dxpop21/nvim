cf704c5f0d07efa18b2486de24ad589dded2d00b
Jesper <>
2025-01-25 17:36:35 -0500

    Customize toggle term

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -1,4 +1,12 @@
 require("toggleterm").setup({
 		open_mapping = [[<c-\>]],
-		-- close_on_exit = true,
+        direction = "float",
+		close_on_exit = true,
+        float_opts = {
+            border = "double",
+            width = 80,
+            height = 30,
+            winblend = 30,
+            shade_terminals = true,
+        }
 })
